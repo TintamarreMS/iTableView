f5966f39fd5a60e3af57690342e8e1caf3a31c83
Martin Guilbaud <tintamarre@MacBook-Air-de-Martin.local>
2015-05-06 10:22:49 +0200

    Initial Commit

---
+++ iTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTableView
+//
+//  Created by Martin Guilbaud on 06/05/2015.
+//  Copyright (c) 2015 tintamarre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
